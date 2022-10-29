0aca39c1ab19057144c7b2576ddc490f7f074595
Sanjula Dulshan <sdulshan10@gmail.com>
2022-10-29 20:14:45 +0530

    init

---
+++ server.js
@@ -0,0 +1,34 @@
+import dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+const PORT = process.env.PORT;
+const MONGODB_URL = process.env.MONGODB_URL;
+
+const app = express();
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(bodyParser.json());
+
+// Connect to MongoDB
+mongoose
+  .connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connected...");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection Failed...", err.message);
+    process.exit();
+  });
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
